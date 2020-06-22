b00daea0399b52af94e4dc9bcb56249dc71df6a4
Diego Garcia <diego_9696c@hotmail.com>
2020-06-23 00:21:22 +0200

    Program working. Time to create the animations

--- ordenacionVisual.js
+++ ordenacionVisual.js
@@ -1,39 +1,57 @@
 'use strict'
 
+/* 
+    Class element whit the span and his random height   
+*/
+class element{
+    span;
+    randomHeight;
+    constructor(span, randomHeight){
+        this.span = span;
+        this.randomHeight = randomHeight;
+    }
+}
+
 /*
-    Creación de los elementos con longitudes aleatorias
+    Create an Array with 1200 spans with random height's
 */
 var arraySpans = [];
-function barajar(){
+var isOrdered = false;
+var isOrderedMessage = false;
+var secondsToSort;
+
+function shuffle(){
     arraySpans = [];
+    isOrdered = false;
+    isOrderedMessage = false;
+
     for(let i = 0; i < 1200; i++){
-        let elemento = [];
         let span = document.createElement("span");
         let randomHeight = Math.floor(Math.random()*101);
         span.style.height = randomHeight + "%";
-        span.id = randomHeight;
-        elemento.push(span);
-        elemento.push(randomHeight);
-        arraySpans.push(elemento);
+        span.id = i;
+        arraySpans.push(new element(span,randomHeight));
     }
 }
 
 function mostrarArray(){
     document.getElementById("container").innerHTML = "";
     for(let i = 0; i < arraySpans.length; i++ ){
-        document.getElementById("container").appendChild(arraySpans[i][0]);
+        document.getElementById("container").appendChild(arraySpans[i].span);
     }
     console.log("Ordenando el array....");
 }
 
 function ordenarBurbuja(){
-    
     let aux;
     let nComparaciones = 0;
+    let date = new Date();
+    let miliBefore = date.getMilliseconds();
+    
     for(let i = 0; i < arraySpans.length - 1; i++){
         for(let j = 0; j < arraySpans.length - 1; j++){
             nComparaciones++;
-            if(arraySpans[j][1] > arraySpans[j + 1][1]){
+            if(arraySpans[j].randomHeight > arraySpans[j + 1].randomHeight){
                 aux = arraySpans[j];
                 arraySpans[j] = arraySpans[j + 1];
                 arraySpans[j + 1] = aux;
@@ -42,17 +60,41 @@ function ordenarBurbuja(){
             }
         }
     }
+    date = new Date();
+    let miliAfter = date.getMilliseconds();
+    isOrdered = true;
+    
+    secondsToSort = (miliAfter - miliBefore)/1000;
+    console.log(`Milis antes: ${miliBefore} - Milis despues: ${miliAfter} - Segundos para ordenar: ${secondsToSort}`);
 }
 
 
-var btnOrdenar = document.getElementById("btnOrdenar");
-btnOrdenar.addEventListener('click', () =>{
-    barajar();
+var btnOrdenar = document.getElementById("btnSort");
+var btnShuffle = document.getElementById("btnShuffle");
+
+btnShuffle.addEventListener('click',() => {
+    document.getElementById("messageContainer").innerHTML = "";
+    btnOrdenar.style.display = "inline";
+    shuffle();
     mostrarArray();
-   
-    ordenarBurbuja();
-    var timeoutID = setTimeout(mostrarArray, 2000);
-    
-    
+});
+
+btnOrdenar.addEventListener('click', () =>{
+    if(!isOrdered){
+        ordenarBurbuja();
+        mostrarArray();
+        // var timeoutID = setTimeout(mostrarArray, 2000);
+        let messageAlreadySorted = document.createElement("p");
+        messageAlreadySorted.id  = "pAlreadySorted";
+        messageAlreadySorted.innerHTML = "Seconds to Sort: " + secondsToSort;
+        document.getElementById("messageContainer").appendChild(messageAlreadySorted);
+    }else{
+        if(!isOrderedMessage){
+            document.getElementById("pAlreadySorted").innerHTML = "Seconds to Sort: " + secondsToSort + " - The array is already sorted."; 
+            isOrderedMessage = true;
+        }
+
+    }
+
 });
 
